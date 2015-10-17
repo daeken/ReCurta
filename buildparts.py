bd6408001858e5cea11afadf9151190ccbd9ccc8
Cody Brocious <cody.brocious@gmail.com>
2015-10-16 21:47:02 -0400

    Added STL viewer to wiki builder.

--- buildparts.py
+++ buildparts.py
@@ -18,7 +18,7 @@ bom = []
 names = dict()
 
 for line in file('bom.txt'):
-	num, name, quantity = [x.strip() for x in line.split('||')][1:-1]
+	num, name, quantity, process = [x.strip() for x in line.split('||')][1:-1]
 	num = int(num.split(' ')[0][3:-3])
 	bom.append((num, name, int(quantity)))
 	names[num] = name
@@ -27,7 +27,13 @@ for num, name, quantity in bom:
 	time.sleep(.25)
 	print 'Creating', num
 
-	body = '[[image Curta_1_%i.jpg size="medium"]]\n\n' % num
+	body = '[[image Curta_1_%i.jpg size="medium"]]\n' % num
+	cfn = '/home/daeken/curta/app/completed/Curta_1_%i.jpg' % num
+	if isfile(cfn):
+		body += '[[image Curta_1_%i_en.jpg size="medium"]]\n\n' % num
+	else:
+		body += '\n'
+	body += '[[iframe http://demoseen.com/curta-stl/?%i width="600" height="450" ]]\n\n' % num
 	if num in referenced:
 		body += '**Referenced by**:\n'
 		for elem in referenced[num]:
@@ -50,14 +56,26 @@ for num, name, quantity in bom:
 	))
 
 	bfn = 'Curta_1_%i.jpg' % num
+	benfn = 'Curta_1_%i_en.jpg' % num
 	fn = '/home/daeken/demoseen/curta/Curta_1_%i.jpg' % num
-	if isfile(fn) and bfn not in s.files.select(dict(site='curtawiki', page=str(num))):
+	cfiles = s.files.select(dict(site='curtawiki', page=str(num)))
+	if isfile(fn) and bfn not in cfiles:
 		time.sleep(1)
 		print 'Uploading', bfn
 		data = file(fn, 'rb').read()
 		s.files.save_one(dict(
 			site='curtawiki', page=str(num), 
 			file=bfn,
+			content=data.encode('base64').replace('\n', ''), 
+			save_mode='create', 
+		))
+	if isfile(cfn) and benfn not in cfiles:
+		time.sleep(1)
+		print 'Uploading', benfn
+		data = file(cfn, 'rb').read()
+		s.files.save_one(dict(
+			site='curtawiki', page=str(num), 
+			file=benfn,
 			content=data.encode('base64').replace('\n', ''), 
 			save_mode='create', 
 		))
