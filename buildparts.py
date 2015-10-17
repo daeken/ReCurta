888cc64838e59d495289decf1e0211a6a499cebf
Cody Brocious <cody.brocious@gmail.com>
2015-10-17 01:09:16 -0400

    Added more process details to BOM.  Added part page region.

--- buildparts.py
+++ buildparts.py
@@ -17,13 +17,26 @@ for parent, children in references.items():
 bom = []
 names = dict()
 
+main_page = """The table below includes every part in the Curta type I.  The original engineering diagrams included on most of these pages are from the [http://www.museummura.li/content.aspx?nid=5051&groupnr=5051 Museum Mura]
+
+||~ Part No. ||~ Name ||~ Quantity ||~ Proposed Process || Has Model ||
+"""
+
+processes = dict(
+	M='CNC Milled', 
+	L='CNC Lathed', 
+	C='Lasercut'
+)
+
 for line in file('bom.txt'):
 	num, name, quantity, process = [x.strip() for x in line.split('||')][1:-1]
 	num = int(num.split(' ')[0][3:-3])
-	bom.append((num, name, int(quantity)))
+	if process in processes:
+		process = processes[process]
+	bom.append((num, name, int(quantity), process))
 	names[num] = name
 
-for num, name, quantity in bom:
+for num, name, quantity, process in bom:
 	time.sleep(.25)
 	print 'Creating', num
 
@@ -35,6 +48,9 @@ for num, name, quantity in bom:
 		body += '\n'
 	if isfile('STL Models/%i.stl' % num):
 		body += '[[iframe http://demoseen.com/curta-stl/?%i width="500" height="500" ]]\n\n' % num
+		main_page += '|| [[[%i]]] || %s || %i || %s || Yes ||\n' % (num, name, quantity, process)
+	else:
+		main_page += '|| [[[%i]]] || %s || %i || %s || ||\n' % (num, name, quantity, process)
 	if num in referenced:
 		body += '**Referenced by**:\n'
 		for elem in referenced[num]:
@@ -80,3 +96,10 @@ for num, name, quantity in bom:
 			content=data.encode('base64').replace('\n', ''), 
 			save_mode='create', 
 		))
+
+s.pages.save_one(dict(
+	site='curtawiki', page='Parts', 
+	title='Parts', 
+	content=main_page, 
+	save_mode='create_or_update'
+))
